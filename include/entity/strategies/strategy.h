01745adbfc9b0006929474b2a80f2831b2df24e3
Clement <mabileau.clement@gmail.com>
2021-12-06 15:12:01 +0100

    important thought about storing strategies in strategy.h commentary

---
+++ include/entity/strategies/strategy.h
@@ -0,0 +1,28 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+virtual class Strategy 
+//For now Strategy seems only a polymorphic-class, usefull only to store every strategies.
+//we'll have to think how we want to use strategies, because if they're all stored as Strategy
+//we can't write strategies.get(0).aMethodFromPhysicalEntity() because strategies.get(0) is of type Strategy,
+//maybe a method in Strategy that return the type of the object(Physical, Drawable,..) or directly return the object ?
+//there is to be discussed here
+
+{
+private:
+    /* data */
+public:
+    Strategy(/* args */);
+    ~Strategy();
+};
+
+Strategy::Strategy(/* args */)
+{
+}
+
+Strategy::~Strategy()
+{
+}
+
+
+#endif
