b2770361fbb54c674bc074db54d29090cc9603dd
Bendi <antoine.bendimerad@icloud.com>
2021-12-20 04:01:11 +0100

    added Callback Functions Handling

--- include/entity/strategies/controllable.h
+++ include/entity/strategies/controllable.h
@@ -2,6 +2,10 @@
 #define CONTROLLABLE_H
 #include "strategy.h"
 #include <SFML/Window.hpp>
+#include <functional>
+#include <string>
+#include <vector>
+#include <map>
 
 #define NB_KEYS 256
 
@@ -10,13 +14,17 @@
 class Controllable : public virtual Strategy
 {
 private:
-    
+    std::vector<std::pair<std::vector<unsigned short>,std::vector<std::function<void()>> > > callBacks; 
+
 public:
     Controllable(/* args */);
     ~Controllable();
 
-    void flush(unsigned short[NB_KEYS] inputs); //flush ? events ?, receive an array of boolean (flags) representing if some keys are pressed, 
+    void flush(std::vector<unsigned short> &inputs); //flush ? events ?, receive an array of boolean (flags) representing if some keys are pressed, 
     // WARNIGN needs a convention (between conductor.ControllableHandler and this class) on that boolean array
+
+    void addCallBack(std::vector<unsigned short> inputs, std::function<void()> callback); // These take effects immediatly ( independant of the MAE )
+
 
 };
 
@@ -27,7 +35,6 @@ Controllable::Controllable(/* args */)
 Controllable::~Controllable()
 {
 }
-
 
 
 
---
+++ src/entity/strategies/controllable.cpp
@@ -0,0 +1,40 @@
+#include "controllable.h"
+
+
+
+
+void Controllable::addCallBack(std::vector<unsigned short> inputs, std::function<void()> callback){
+    
+    for (int i = 0; i < callBacks.size(); i++)
+    {
+        if(callBacks[i].first == inputs){ /// adds the callback to the callBacks Vector
+            callBacks[i].second.emplace_back(callback);
+            return;
+        }
+    }
+
+
+    std::pair<std::vector<unsigned short>, std::function<void()> > a = 
+                                std::pair<std::vector<unsigned short>, std::function<void()>>(inputs,callback);
+
+    callBacks.emplace_back(a);
+    
+}
+
+void Controllable::flush(std::vector<unsigned short> &inputs){
+
+    // Calls all callback functions ( without MAE)
+    for (int i = 0; i < callBacks.size(); i++)
+    {
+        if(callBacks[i].first == inputs){
+            for (int j = 0; j < callBacks[i].second.size(); i++)
+            {
+                callBacks[i].second[j](); // flushes Every Callbacks
+            }
+            break;
+        }
+    }
+    
+    
+
+}
