fe073884e5b473277551445d6edb9c19fc4f9ad5
matt <bassetmatt@gmail.com>
2022-01-12 18:24:55 +0100

    Adjustments for entity displacement

--- src/entity/physical_entity.cpp
+++ src/entity/physical_entity.cpp
@@ -1,8 +1,8 @@
 class PhysicalEntity {
 
 struct collisionner {
-    Entity* touched;
     Entity* touching;
+    Entity* touched;
     std::vector<int> intricate;
 };
 
@@ -11,18 +11,20 @@ typedef struct collisionner collisionner;
 static void correctionUpdate(std::vector<collisionner> collisionners)
 {
     for (collisionner col : collisionners) {
-        Entity ett1 = *col.touched;
-        Entity ett2 = *col.touching;
+        Entity ett1 = *col.touching;
+        Entity ett2 = *col.touched;
         std::vector<int> intr = col.intricate;
-        unsigned double m1,m2; //mass
+        unsigned float m1,m2; //mass
+        //TODO implement bouncyness later
         // unsigned double b1,b2; //bouncyness
         // b1 = ett1.bouncyness;
         // b2 = ett2.bouncyness;
         m1 = ett1.mass;
         m2 = ett2.mass;
-        unsigned double ratio = m1/(m1+m2);
-        ett1.translate(-ratio*intr);
-        ett2.translate((1-ratio)*intr);
+        const unsigned float ratio = m2/(m1 + m2);
+        // Inherited from SFML transformable
+        ett1.move(floor( ratio     *intr[0]),floor( ratio     *intr[1]));
+        ett2.move(floor((ratio - 1)*intr[0]),floor((ratio - 1)*intr[1]));
     }
 }
 }
