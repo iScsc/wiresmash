4d5f64ea37fe7fc7043430ec9b93c54e4b64e528
matt <bassetmatt@gmail.com>
2022-01-12 19:41:52 +0100

    Should work

--- src/entity/physical_entity.cpp
+++ src/entity/physical_entity.cpp
@@ -1,30 +1,59 @@
 class PhysicalEntity {
 
-struct collisionner {
-    Entity* touching;
-    Entity* touched;
-    std::vector<int> intricate;
-};
+private :
+    PhysicsProp phxProp;
 
-typedef struct collisionner collisionner;
 
-static void correctionUpdate(std::vector<collisionner> collisionners)
-{
-    for (collisionner col : collisionners) {
-        Entity ett1 = *col.touching;
-        Entity ett2 = *col.touched;
-        std::vector<int> intr = col.intricate;
-        unsigned float m1,m2; //mass
-        //TODO implement bouncyness later
-        // unsigned double b1,b2; //bouncyness
-        // b1 = ett1.bouncyness;
-        // b2 = ett2.bouncyness;
-        m1 = ett1.mass;
-        m2 = ett2.mass;
-        const unsigned float ratio = m2/(m1 + m2);
-        // Inherited from SFML transformable
-        ett1.move(floor( ratio     *intr[0]),floor( ratio     *intr[1]));
-        ett2.move(floor((ratio - 1)*intr[0]),floor((ratio - 1)*intr[1]));
+public :
+    /*
+    * Collisionner, structure defining the collision between 2 entities
+    */
+    struct Collisionner {
+        Entity* touching;
+        Entity* touched;
+        std::vector<int> intricate;
+    };
+
+    typedef struct Collisionner Collisionner;
+
+    /*
+    * Physics properties structure of a given entity
+    */
+    struct PhysicsProp {
+        std::vector<int> position;
+        std::vector<int> speed;
     }
-}
+
+    typedef struct PhysicsProp PhysicsProp;
+
+    /*
+    * Inverts the speed
+    */
+    void invertSpeed() {
+        phxProp.speed[0] *= -1;
+        phxProp.speed[1] *= -1;
+    }
+
+    static void correctionUpdate(std::vector<Collisionner> collisionners)
+    {
+        for (Collisionner col : collisionners) {
+            Entity ett1 = *col.touching;
+            Entity ett2 = *col.touched;
+            std::vector<int> intr = col.intricate;
+            //TODO implement bouncyness later
+            // unsigned double b1,b2; //bouncyness
+            // b1 = ett1.bouncyness;
+            // b2 = ett2.bouncyness;
+            const unsigned int m1 = ett1.mass;
+            const unsigned int m2 = ett2.mass;
+            const unsigned float ratio = m2/(m1 + m2);
+            // Inherited from SFML transformable
+            ett1.move(floor( ratio     *intr[0]),floor( ratio     *intr[1]));
+            ett2.move(floor((ratio - 1)*intr[0]),floor((ratio - 1)*intr[1]));
+
+            //Reversing speed
+            ett1.invertSpeed();
+            ett2.invertSpeed();
+        }
+    }
 }
