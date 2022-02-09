73ead5b28c8449678ad8f35c76e90d7a65407915
matt <bassetmatt@gmail.com>
2022-02-09 19:11:34 +0100

    Starting working on main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,29 @@
 using namespace std;
 
 #include <SFML/Graphics.hpp>
-
 #include "conductor/Conductor.h"
 #include "entity/Character.h"
 
-
 int main(){
-  sf::RenderWindow window(sf::VideoMode(800, 450), "fil rouge!");
-  window.setVerticalSyncEnabled(true);
-  window.setFramerateLimit(60);
-
-
+    sf::RenderWindow window(sf::VideoMode(800, 450), "fil rouge!");
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
+    vector<Force> forces {Force::weight};
+    PhysicalEntity ett1 = PhysicalEntity(forces);
+    while (window.isOpen()){
+        sf::Event event;
+        while (window.pollEvent(event)){
+            switch (event.type){
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyReleased:
+                    // TODO
+                    break;
+                default:
+                    break;
+            }
+        }
 
   Conductor conductor;
 
@@ -30,14 +42,7 @@ int main(){
           break;
       }
     }
-
-    // objects update.
-    conductor.flush();
-    window.clear(sf::Color::Black);
-    // TODO
-    window.display();
-  }
-  
-  return 0;
+    
+    return 0;
 }
 
