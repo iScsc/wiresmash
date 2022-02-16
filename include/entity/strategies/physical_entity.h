85a918f5c52fdda46aac1abb0efa8cc80c90e736
Akainoru <PIXELtmGamer@gmail.com>
2022-02-16 18:58:59 +0100

    quelques modif sur physical_entity.h/.cpp

--- include/entity/strategies/physical_entity.h
+++ include/entity/strategies/physical_entity.h
@@ -69,28 +69,4 @@ public:
 
 };
 
-// in cpp :
-PhysicalEntity::PhysicalEntity(/* Default values */) : Strategy(/* no args */), sf::Transformable()
-{
-    // Initialize default parameters
-    this->mass = 100;
-    this->gravityReaction = 1;
-    this->bouncyness = .5;
-    this->internalEnergy = 0;
-    this->electricCharge = 0;
-    // Done for now, depending on args
-}
-
-PhysicalEntity::PhysicalEntity(unsigned double mass, unsigned double bouncyness, signed short gravityReaction, double internalEnergy, double electricCharge) : Strategy(/* no args */), sf::Transformable() {
-    this->mass = mass;
-    this->gravityReaction = gravityReaction;
-    this->bouncyness = bouncyness;
-    this->internalEnergy = internalEnergy;
-    this->electricCharge = electricCharge;
-}
-
-PhysicalEntity::~PhysicalEntity()
-{
-}
-
 #endif
