cb142cce528ae0cb38007ff340e83a288d84bc41
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-03-02 17:48:13 +0100

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
-#include "entity/entity.h"
-#include "entity/strategies/strategy.h"
 #include <iostream>
-using namespace std;
-
-#include "entity/strategies/physical_entity.h"
-#include "phx/phx.h"
 #include <SFML/Graphics.hpp>
 
+#include "entity/entity.h"
+#include "entity/strategies/strategy.h"
+#include "entity/strategies/physical_entity.h"
+#include "phx/phx.h"
+
+using namespace std;
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 450), "fil rouge!");
+  sf::RenderWindow window(sf::VideoMode(800, 450), "Wiresmash");
   window.setVerticalSyncEnabled(true);
   window.setFramerateLimit(60);
-  vector<Force> forces = {Force::weight};
-  std::vector<Strategy*> weightEtt = { nullptr, new PhysicalEntity(forces), nullptr}; // TODO so ugly
+  PhyParam param = PhyParam(1);
+  std::vector<Strategy*> weightEtt = { nullptr, new PhysicalEntity(param), nullptr}; // TODO so ugly
   Entity *ett1 = new Entity(weightEtt);
   Entity *ett2 = new Entity(weightEtt);
 
@@ -34,10 +35,12 @@ int main() {
     // objects update.
     window.clear(sf::Color::Black);
 
-    vel vel1 = updateCinematics((PhysicalEntity*) ett1->getStrategy(PHYSICAL));
-    ett1->addVelocity(vel1);
-    vel vel2 = updateCinematics((PhysicalEntity*) ett1->getStrategy(PHYSICAL));
-    ett2->addVelocity(vel2);
+    vel acc1 = updateCinematics((PhysicalEntity*) ett1->getStrategy(PHYSICAL));
+    ett1->incVelocity(acc1);
+    ett1->addVelocity();
+    vel acc2 = updateCinematics((PhysicalEntity*) ett2->getStrategy(PHYSICAL));
+    ett2->incVelocity(acc2);
+    ett2->addVelocity();
     
     std::cout << "position of ett1: " << ett1->getPosition().x << ";" << ett1->getPosition().y << std::endl;
     std::cout << "position of ett2: " << ett2->getPosition().x << ";" << ett2->getPosition().y << std::endl;
