a4d0c8acaffff7209a29e5c01634af979f6cb04b
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-03-12 19:19:18 +0100

    cleanup of multi drawable

--- include/entity/strategies/multi_drawable.h
+++ include/entity/strategies/multi_drawable.h
@@ -1,23 +1,24 @@
 #ifndef MULTI_DRAWABLE
 #define MULTI_DRAWABLE
-#include "strategy.h"
 #include <vector>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
-#include <vector>
+#include "entity/strategies/strategy.h"
 
 class MultiDrawable : public sf::Drawable/*a MultiDrawable can be in toDraw of another MultiDrawable*/, public virtual Strategy {
 private:
     std::vector<sf::Drawable*> toDraw; //list of sprites/shapes to draw
     //!!! big question on the best way to store Sprites ? and on the pointer and freeing...
 public:
-    MultiDrawable();
+
+    MultiDrawable() = default;
+    MultiDrawable(sf::Drawable*);
     MultiDrawable(std::vector<sf::Drawable*>);
-    MultiDrawable(sf::Drawable);
-    ~MultiDrawable();
+
     void draw(sf::RenderTarget& target/*often the window where to draw*/, 
               sf::RenderStates states/*shader or other*/);
-    void addSprite(sf::Drawable);//called addSprite but can add any sf::Drawable
+    
+    void addSprite(sf::Drawable*);//called addSprite but can add any sf::Drawable
 };
 
 #endif
--- src/entity/strategies/multi_drawable.cpp
+++ src/entity/strategies/multi_drawable.cpp
@@ -1,29 +1,16 @@
 #include "entity/strategies/multi_drawable.h"
 
-MultiDrawable::MultiDrawable(/* no args */) : Strategy(/* no args */), sf::Drawable()
-{
-    // Done for now
-}
-
-MultiDrawable::~MultiDrawable(){
-
-}
-
 void MultiDrawable::draw(sf::RenderTarget& target, sf::RenderStates states) {
     for(sf::Drawable* d : toDraw){
         target.draw(*d,states);
     }
 }
 
-void MultiDrawable::MultiDrawable(std::vector<sf::Drawable*> &spriteVector){
-    this->toDraw.swap();//normally free the vector, but don't free the sprites where pointers in it point to
+MultiDrawable::MultiDrawable(std::vector<sf::Drawable*> spriteVector){
+    this->toDraw.clear();//normally free the vector, but don't free the sprites where pointers in it point to
     this->toDraw = spriteVector;
 }
 
-void MultiDrawable::MultiDrawable(sf::Drawable *sprite){
+MultiDrawable::MultiDrawable(sf::Drawable *sprite){
     this->addSprite(sprite);
-}
-
-void MultiDrawable(){
-    //would it be good by default to add a "default sprite" to avoid risk of crashed when draw on empty vector
 }
