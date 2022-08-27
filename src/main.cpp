c1204b8007d5504fbd6810ce63b382a3d6f53e76
moi <gbriva17@gmail.com>
2022-08-27 02:43:16 +0200

    Implement collision demonstration

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 #include "engine/universe_master.h"
 #include "graphics/display.h"
+
 #include "game/entity/dev/red_square.h"
+#include "engine/hitbox/rectangle.h"
 
 int main(int argc, char const *argv[]){
 
@@ -15,10 +17,27 @@ int main(int argc, char const *argv[]){
     universe.linkWindow(&window);
 
     RedSquare* dev_ett1 = new RedSquare();
+    dev_ett1->addHitbox(new Rectangle(10,10));
 
     universe.addEntity(dev_ett1);
     // universe.addEntity(entity2);
 
+    Entity* rect1 = new Entity("Square1");
+    rect1->setPos(std::pair<int, int>(13, -39));
+    rect1->addHitbox(new Rectangle(5,5));
+    sf::RectangleShape r1 = sf::RectangleShape(sf::Vector2f(5,5));
+    rect1->addSprite(&r1);
+
+    Entity* rect2 = new Entity("Square2");
+    rect2->setPos(std::pair<int, int>(9, -40));
+    rect2->addHitbox(new Rectangle(5,5));
+    sf::RectangleShape r2 = sf::RectangleShape(sf::Vector2f(5,5));
+    r2.setFillColor(sf::Color(0,255,0));
+    rect2->addSprite(&r2);
+
+
+    universe.addEntity(rect1);
+    universe.addEntity(rect2);
 
     while(window.isOpen()){
 
