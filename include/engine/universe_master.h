425fb1b65baef7ab3b4c2fa0190201285f71564e
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-03 20:33:53 +0100

    Document the universe

--- include/engine/universe_master.h
+++ include/engine/universe_master.h
@@ -1,3 +1,12 @@
+/**
+ * @file universe_master.h
+ * @author Grégory Brivady
+ * @brief 
+ * @version 0.1
+ * @date 2022-12-03
+ * 
+ * 
+ */
 #ifndef ENGINE_UNIVERSE_MASTER_H
 #define ENGINE_UNIVERSE_MASTER_H
 
@@ -30,9 +39,27 @@ struct Keybind //TODO : move this to input reader
     UniversalInput    action;
 };
 
-
+/**
+ * @brief Conductor of the engine
+ * 
+ * @details Stores all the entity and every property as a stack.
+ * Then handles the iteration over every stack, at each frame,
+ * of every "property system".
+ * 
+ */
 class UniverseMaster
 {
+public:
+    UniverseMaster(/* args */) = default;
+
+    void linkWindow(sf::RenderWindow* p_window){this->window = p_window;};
+
+    void addEntity(Entity* entity);
+
+    void update();
+
+    void initInput(); //TODO : Temporary for dev, should be initialised properly
+
 private:
     std::list<Keybind> keybindings;
 
@@ -52,16 +79,6 @@ private:
 
     sf::RenderWindow* window;
 
-public:
-    UniverseMaster(/* args */) = default;
-
-    void linkWindow(sf::RenderWindow* p_window){this->window = p_window;};
-
-    void addEntity(Entity* entity);
-
-    void update();
-
-    void initInput(); //TODO : Temporary for dev, should be initialised properly
 };
 
 
