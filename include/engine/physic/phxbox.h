9b68c2a53d2763e263bdae6b7244286ab282edae
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-03 20:13:08 +0100

    Document Physical (Collision-like) behaviour

--- include/engine/physic/phxbox.h
+++ include/engine/physic/phxbox.h
@@ -1,3 +1,12 @@
+/**
+ * @file phxbox.h
+ * @author Grégory Brivady
+ * @brief Defines the behaviour of boxes that are supposed to work under the laws of physics
+ * @version 0.1
+ * @date 2022-12-03
+ * 
+ * 
+ */
 #ifndef ENGINE_PHYSIC_PHXBOX_H
 #define ENGINE_PHYSIC_PHXBOX_H
 
@@ -6,10 +15,21 @@
 
 class Entity;
 
+/**
+ * @brief Boxes that should push each others to solve intersection.
+ * 
+ */
 class PhxBox{
     friend Entity;
 public:
-    enum class CollisionBehaviour {PUSHABLE, STATIC};
+    /**
+     * @brief Sub-behaviour to solve collisions
+     * 
+     */
+    enum class CollisionBehaviour {
+        PUSHABLE, ///< Can be pushed by another box
+        STATIC    ///< Cannot be pushed
+    };
     PhxBox() = default;
 
     Box<PhxBox>* getBox() {return this->box;};
@@ -18,6 +38,18 @@ public:
     void linkOwner(Entity* owner);
     void linkBox(Box<PhxBox>* box);
 
+    /**
+     * @brief [TEMPORARY] Solve the collision between two objects
+     * 
+     * @details Should be called twice to solve everything.
+     * If both object are @see CollisionBehaviour::PUSHABLE then they each
+     * get pushed by half of the intersection vector, in a direction that solves 
+     * the collision. If one of them is STATIC, then it does not move and the 
+     * other one does by the full intersection vector. If both are STATIC, 
+     * nothing happens.
+     * 
+     * @param coll_pb 
+     */
     void solveCollision(Intersection<PhxBox>& coll_pb);
 
     Entity* getOwner();
