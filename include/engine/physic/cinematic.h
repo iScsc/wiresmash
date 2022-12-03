f3269cee97026e66c46449c726c6d9409b7d904f
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-03 19:37:55 +0100

    Document science

--- include/engine/physic/cinematic.h
+++ include/engine/physic/cinematic.h
@@ -1,3 +1,11 @@
+/**
+ * @file cinematic.h
+ * @author Grégory Brivady
+ * @brief Contains anything that may be useful to do point cinematic for the physic
+ * @version 0.1
+ * @date 2022-12-03
+ * 
+ */
 #ifndef ENGINE_PHYSIC_CINEMATIC_H
 #define ENGINE_PHYSIC_CINEMATIC_H
 
@@ -5,6 +13,12 @@
 
 #define PHX_CST_G -1
 
+/**
+ * @brief Gravity.
+ * 
+ * @param vel 
+ * @param mass 
+ */
 void gravity(std::pair<double, double>* vel, double* mass);
 
 #endif
