a608e27f9a5c97856cdf24d7f6d8e8b55f8c2712
g.brivady <12777915+gbrivady@users.noreply.github.com>
2022-12-04 03:12:38 +0100

    Document input reader from file

--- include/settings/input_reader.h
+++ include/settings/input_reader.h
@@ -1,3 +1,12 @@
+/**
+ * @file input_reader.h
+ * @author Grégory Brivady
+ * @brief Read keybinds from a file
+ * @version 0.1
+ * @date 2022-12-04
+ * 
+ * 
+ */
 #ifndef SETTINGS_INPUT_READER_H
 #define SETTINGS_INPUT_READER_H
 
@@ -15,10 +24,28 @@ struct Keybind //TODO : move this to input reader
     UniversalInput    action;
 };
 
+/**
+ * @brief make a list of keybinds, given a file
+ * 
+ * @param path input file
+ * @return std::list<Keybind> 
+ */
 std::list<Keybind> make_keybinds(std::string path);
 
+/**
+ * @brief Read the players part of the inputs
+ * 
+ * @param keybinds 
+ * @param file 
+ */
 void read_player_input(std::list<Keybind>& keybinds, std::ifstream& file);
 
+/**
+ * @brief Transforms a string to an sfml key
+ * 
+ * @param str 
+ * @return sf::Keyboard::Key 
+ */
 sf::Keyboard::Key key_of_string(std::string str);
 
 #endif
